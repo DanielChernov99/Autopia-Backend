74e8d4fda0ee3b03fb77905c4d7fea4adcbe496f
Andrey Ruch <andreyruch1@gmail.com>
2026-08-12 12:16:34 +0300

    feat: connect to database and update server initialization
    - Import and invoke connectDB to establish a database connection
    - Remove unused .gitkeep file from config directory

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 3000;
+
+await connectDB();
 
 app.listen(PORT, () => {
   console.log(`Autopia API is running at http://localhost:${PORT}`);
