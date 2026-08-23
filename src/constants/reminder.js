2541732e899ace10ac26d2ad2398c3ed65b33a79
Andrey Ruch <andreyruch1@gmail.com>
2026-08-23 09:25:28 +0300

    refactor: update reminder frequency constants to use an object for improved clarity

--- src/constants/reminder.js
+++ src/constants/reminder.js
@@ -1,3 +1,5 @@
 export const REMINDER_TYPES = ["test", "insurance"];
 
-export const REMINDER_FREQUENCIES = ["yearly", "halfYearly"];
+export const REMINDER_FREQUENCY_MONTHS = { yearly: 12, halfYearly: 6 };
+
+export const REMINDER_FREQUENCIES = Object.keys(REMINDER_FREQUENCY_MONTHS);
